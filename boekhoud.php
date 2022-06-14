76b95aad531f69f3426c19df6cafd04f33c97c6a
TimTillekens <1043001@mborijnland.nl>
2022-06-14 16:40:57 +0200

    namen aangepast

--- boekhoud.php
+++ boekhoud.php
@@ -18,7 +18,7 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 }
 
 //hier word de tabel uitgezet
-if(isset($_POST['subritUit'])){
+if(isset($_POST['boekhoudUit'])){
     $sql = "DELETE TABLE boekhoud";}
 ?>
 
@@ -32,7 +32,7 @@ if(isset($_POST['subritUit'])){
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<h2>Subrit module beheer</h2>
+<h2>boekhoud module beheer</h2>
 	<div class="formelier">
 		<!-- Dit is het formelier waar je kan aangen of je een tabel wilt toevoegen of verwijderen -->
 		<form action="index.php" method="post">
@@ -40,10 +40,10 @@ if(isset($_POST['subritUit'])){
 			<input type="text" name="databaseName" placeholder="Naam Database" class="klantnaam">
 		
 			<input type="checkbox" name="boekhoud" value="1" class="form-check-input">
-			<label>Subrit module aan</label>
+			<label>boekhoud module aan</label>
 
-			<input type="checkbox" name="subritUit" value="1" class="form-check-input">
-			<label>Subrit module uit</label>
+			<input type="checkbox" name="boekhoudtUit" value="1" class="form-check-input">
+			<label>boekhoud module uit</label>
 		<div class="knop">
         <button type="submit" name="submit" class="knopje">Opslaan</button><br>
         <button name="terug" class="knopjes"><a href="index.php">Terug</a></button>
