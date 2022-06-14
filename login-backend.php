1408faa86683c405d81c2c7163922332c09ae4c9
TimTillekens <1043001@mborijnland.nl>
2022-06-14 15:20:21 +0200

    Update login-backend.php

--- login-backend.php
+++ login-backend.php
@@ -3,3 +3,12 @@
 session_start(); 
 //hier word de database connectie toegevoegd aan de login page
 include "db.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
