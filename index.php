79f6b0d3e5f593d844bccdda74ac61461c0137c8
TimTillekens <1043001@mborijnland.nl>
2022-06-14 11:40:28 +0200

    buttons toegevoed
    Ik heb de buttons toe gevoed om naar de andere pagina's te gaan.

--- index.php
+++ index.php
@@ -11,10 +11,17 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Database Aanamken</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/sty.css">
 	<!-- Bootstrap css -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+<!-- hier staat code om de pagina te stylen -->
+<style type="text/css">
+	a {
+		text-decoration: none;
+		color: white;
+	}
+</style>
 <body>
 	<!-- titel van de pagina -->
 	<h2>Klant systeem aanmaken</h2>
@@ -41,6 +48,14 @@
 			<div class="knop">
 				<!-- hier is de submit knop -->
        			<button type="submit" name="submit" class="knopje">Maak aan</button>
+    	</div>
+    	<!-- Hier staan de linkjes naar de paginas waar je tabllen in de database kaan aan/uitzetten -->
+    	<div class="knop">
+        <button name="subrit" class="knopjes"><a href="subrit.php"> Subrit beheer</a></button>
+        <button name="boekhoud" class="knopjes"><a href="boekhoud.php">boekhoud beheer</a></button>
+        <button name="kalender" class="knopjes"><a href="kalender.php">kalender beheer</a></button>
+        <button name="artikel" class="knopjes"><a href="artikelen.php">Artikelen beheer</a></button><br>
+        <button name="loguit" class="knopjes1">log uit</button>
     	</div>
 		</form>
 	</div>
